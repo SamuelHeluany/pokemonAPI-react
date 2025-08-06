48b620eadb033f53d2af0fe137909162a5c75d15
Samuel Heluany <samuelheluany@hotmail.com>
2025-08-06 15:59:24 -0300

    end css background and css SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -10,7 +10,7 @@ export const SearchBar = () => {
         </button>
 
         <div className='search'>
-        <input type="search" name="searchInput" className='searchInput' />
+        <input type="search" name="searchInput" className='searchInput' placeholder='Digite o nome do pokemon' />
         <button className='buttonSearch'>
             <img src={Search} />
         </button>
