7d346766566433b4f3f5c603624ef6be8f0f4bcc
Samuel Heluany <samuelheluany@hotmail.com>
2025-08-06 17:28:20 -0300

    end css project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import PokemonsCharacters from './components/PokemonsCharacters/PokemonsCharacters'
 import { SearchBar } from './components/SearchBar/SearchBar'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <header>
         <SearchBar />
       </header>
+      <PokemonsCharacters />
     </>
   )
 }
--- src/components/PokemonsCharacters/PokemonsCharacters.jsx
+++ src/components/PokemonsCharacters/PokemonsCharacters.jsx
@@ -1,9 +1,36 @@
 import './PokemonsCharacters.css'
+import Charm from '../../assets/charm.png'
+import Category from '../../assets/category.svg'
+import Attack from '../../assets/sword.svg'
+import Shield from '../../assets/shield.svg'
+import Speed from '../../assets/speed.svg'
+import Hp from '../../assets/hp.svg'
 
-const PokemonGrid = () => {
+
+
+
+
+
+const PokemonsCharacters = () => {
   return (
-    <div>PokemonGrid</div>
+      <div className="pokemon">
+          <div className="pokemon-stats">
+            <ul>
+            <li>Nome: Pokemon name</li>
+            <li><img src={Attack} />Ataque: 50</li>
+            <li><img src={Shield} />Defesa: 50</li>
+            <li><img src={Speed} />Velocidade: 50</li>
+            <li><img src={Hp} />Vida: 50</li>
+            <li><img src={Category} />Categoria: Fantasma</li>
+            </ul>
+            
+          </div>
+        
+        <div className="pokemon-image">
+            <img src={Charm}/>
+        </div>
+        </div>
   )
 }
 
-export default PokemonGrid
+export default PokemonsCharacters
