92b8c86fe1eb3e672e71a172c82f618289955d63
Samuel Heluany <samuelheluany@hotmail.com>
2025-08-12 09:55:24 -0300

    Add react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import { Pokemons } from './components/Pokemons/Pokemons.jsx'
+import PokemonsCharacters from './components/PokemonsCharacters/PokemonsCharacters.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Pokemons />
+  },
+   {
+    path: 'pokemonInfo',
+    element: <PokemonsCharacters />
+   }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
